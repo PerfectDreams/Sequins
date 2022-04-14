66dba805ea82cecace22bcfa0ac9df7f60de4c8f
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-04-14 20:06:13 -0300

    Use Gradle's PasswordCredentials

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,10 +29,7 @@ subprojects {
                 name = "PerfectDreams"
                 url = uri("https://repo.perfectdreams.net/")
 
-                credentials {
-                    username = System.getProperty("USERNAME") ?: System.getenv("USERNAME")
-                    password = System.getProperty("PASSWORD") ?: System.getenv("PASSWORD")
-                }
+                credentials(PasswordCredentials::class)
             }
         }
     }
